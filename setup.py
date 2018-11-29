8be26722b8f5d47c5f6b21a6bd460dd254a9a1b4
Edwin Tye <edwin.tye@gmail.com>
2018-11-29 22:16:42 +0000

    grab version programmatically

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 #!/bin/env python
-'''
+"""
 @author: Edwin Tye (Edwin.Tye@phe.gov.uk)
-'''
+"""
+import re
+import subprocess
 from setuptools import setup
 
-with open('LICENSE.txt') as f:
+with open('LICENSE.txt', 'r') as f:
     license_file = f.read()
     
-with open('README.rst') as f:
+with open('README.rst', 'r') as f:
     readme = f.read()
 
-cmdclass = {}
-ext_modules =[]
+version = subprocess.check_output(["git", "describe"]).strip().decode()
+version = re.match(r'v(\d\.\d\.\d).*', version).group(1)
+
 install_requires = [
     'dask>=0.13.0',
     'matplotlib>=1.0.0',
@@ -23,23 +26,22 @@ install_requires = [
     'sympy>=1.0.0'
 ]
 
-setup(name='pygom',
-      version='0.1.3',
-      description='ODE modeling in Python',
-      long_description=readme,
-      license=license_file,
-      url='https://github.com/PublicHealthEngland/pygom',
-      author="Edwin Tye",
-      author_email="Edwin.Tye@phe.gov.uk",
-      packages=[
-          'pygom',
-          'pygom.model',
-          'pygom.loss',
-          'pygom.utilR'
-      ],
-      install_requires=install_requires,
-      cmdclass=cmdclass,
-      ext_modules=ext_modules,
-      test_suite='tests',
-      scripts=[]
-)
+setup(
+    name='pygom',
+    version=version,
+    description='ODE modeling in Python',
+    long_description=readme,
+    license=license_file,
+    url='https://github.com/PublicHealthEngland/pygom',
+    author="Edwin Tye",
+    author_email="Edwin.Tye@phe.gov.uk",
+    packages=[
+        'pygom',
+        'pygom.model',
+        'pygom.loss',
+        'pygom.utilR'
+    ],
+    install_requires=install_requires,
+    test_suite='tests',
+    scripts=[]
+    )
