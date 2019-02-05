4c90775a2d68f82fb48fcaf46ebc9bfa1931c50e
Thomas Finnie <Thomas.Finnie@phe.gov.uk>
2019-02-05 15:16:53 +0000

    Add the master datamodel for a patch model

---
+++ patch_model.py
@@ -0,0 +1,18 @@
+'''
+Created on 5 Feb 2019
+
+@author: thomas.finnie
+'''
+
+import uuid
+
+from schematics.models import Model
+from schematics.types.compound import ModelType, ListType
+from .model import DeterministicOdeModel
+from .movement import Flow
+from .patch import Patch
+
+class PatchModel(Model):
+    patches = ListType(ModelType(Patch))
+    flows = ListType(ModelType(Flow))
+    template_model = ModelType(DeterministicOdeModel)
