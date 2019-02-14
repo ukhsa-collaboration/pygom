e93655a8e9cb2862909cfd87de27614ee8259f3e
Thomas Finnie <Thomas.Finnie@phe.gov.uk>
2019-02-14 13:31:20 +0000

    Update the exports from the datamodel

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
 from .movement import Flow, Flux, Appointment
 from .patch import Patch
+from .model import DeterministicOdeModel, ParameterValue, ParameterValueList
+from .model_run import PsudoPatchRun, StateReport
 
 __all__ = [
     'Flow',
     'Flux',
     'Appointment',
-    'Patch'
+    'Patch',
+    'PsudoPatchRun',
+    'StateReport',
+    'DeterministicOdeModel',
+    'ParameterValue',
+    'ParameterValueList'
     ]
