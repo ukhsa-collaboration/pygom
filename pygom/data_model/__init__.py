af4a0fe864eeace9f4706b1aee975dcd41f87e04
Thomas Finnie <Thomas.Finnie@phe.gov.uk>
2019-08-20 16:21:11 +0100

    Add initial datamodel components from quilty

---
+++ pygom/data_model/__init__.py
@@ -0,0 +1,15 @@
+from .model import (DeterministicOdeModel,
+                    ParameterValue,
+                    ParameterValueList,
+                    StateValue
+                    )
+from .metadata import MetaData
+
+__all__ = [
+    'DeterministicOdeModel',
+    'ParameterValue',
+    'ParameterValueList',
+    'PatchModel',
+    'StateValue',
+    'MetaData'
+    ]
